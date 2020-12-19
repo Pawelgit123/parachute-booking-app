f6fd28843771b8971d6ec88fe5d83335f0f090b0
TenoEiss <lukasz12323@gmail.com>
2020-12-19 03:18:07 -0800

    initial project

---
+++ src/test/java/com/parachute/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parachute.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
