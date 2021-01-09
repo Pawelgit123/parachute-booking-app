b2b3d522e4359e5c3273a4cc2c1f30d173f76166
TenoEiss <lukasz12323@gmail.com>
2021-01-09 05:29:01 -0800

    removed unnecessary comment

--- src/main/java/com/parachute/booking/ApplicationConfiguration.java
+++ src/main/java/com/parachute/booking/ApplicationConfiguration.java
@@ -1,8 +1,5 @@
 package com.parachute.booking;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,12 +9,6 @@ import java.time.Duration;
 
 @Configuration
 public class ApplicationConfiguration {
-//    @Bean
-//    ObjectMapper getObjectMapper() {
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//        return objectMapper;
-//    }
 
     @Bean
     RestTemplate getRestTemplate() {
