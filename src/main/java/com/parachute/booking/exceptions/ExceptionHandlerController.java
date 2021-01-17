01b80460d921480d015e567dbf83ab88c71dd307
Pawelgit123 <60837096+Pawelgit123@users.noreply.github.com>
2021-01-17 14:12:02 +0100

    @RestControllerAdvice

--- src/main/java/com/parachute/booking/exceptions/ExceptionHandlerController.java
+++ src/main/java/com/parachute/booking/exceptions/ExceptionHandlerController.java
@@ -4,14 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class ExceptionHandlerController {
 
