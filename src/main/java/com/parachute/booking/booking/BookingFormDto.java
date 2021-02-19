1b8a9f1bccecca220e5d88a185729e3f22dc0886
TenoEiss <lukasz12323@gmail.com>
2021-02-19 08:32:42 -0800

    refactor

--- src/main/java/com/parachute/booking/booking/BookingFormDto.java
+++ src/main/java/com/parachute/booking/booking/BookingFormDto.java
@@ -1,22 +1,15 @@
 package com.parachute.booking.booking;
 
 import com.parachute.booking.client.ClientDto;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingFormDto {
     private LocalDateTime plannedFlightDateTime;
     private ClientDto clientDto;
-
-    public BookingFormDto(LocalDateTime plannedFlightDateTime, ClientDto clientDto) {
-        this.plannedFlightDateTime = plannedFlightDateTime;
-        this.clientDto = clientDto;
-    }
-
-    public BookingFormDto() {
-    }
 }
